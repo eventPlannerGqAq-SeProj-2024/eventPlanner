53136204890d86f4ff1e12bbe309cb437f352ff5
Ghassan M. Qasrawi <159701545+Qasrawi03Ghassan@users.noreply.github.com>
2024-02-24 22:49:26 +0200

    Admin login done in main

--- src/main/java/event/Planner/adminLoginMain.java
+++ src/main/java/event/Planner/adminLoginMain.java
@@ -4,38 +4,80 @@ import java.util.*;
 
 public class adminLoginMain {
 		
-	public static boolean checkAdminPass(String pass) {
+	public static ArrayList<String> adminNames = new ArrayList<String>();
+	public static ArrayList<String> adminIDs = new ArrayList<String>();
+	public static ArrayList<String> adminPasswords = new ArrayList<String>();
+	
+	public static boolean checkAdminPass(String ID,String pass) {
 		
-		for(int i=0;i<passwords.size();i++) {
-			if(pass.equals(passwords.get(i))) {
+		for(int i=0;i<adminPasswords.size();i++) {
+			if(pass.equals(adminPasswords.get(i)) && (adminIDs.indexOf(ID) == adminPasswords.indexOf(pass))) {
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public static ArrayList<String> passwords = new ArrayList<String>();
+	public static String getName(String ID) {
+			
+			for(int i=0;i<adminIDs.size();i++) {
+				if(adminIDs.indexOf(ID) == i) {
+					return adminNames.get(i);
+				}
+			}
+			
+			return "NaN";
+	}
 	
 	public static void main(String args[]) {
 		
+		adminNames.add("Ali");
+		adminNames.add("Ahmad");
+		adminNames.add("Ruba");
+		adminNames.add("Ghassan");
+		adminNames.add("Adel");
+		adminNames.add("Malak");
+		adminNames.add("Toqa");	
 		
-		passwords.add("admin1");
-		passwords.add("admin2");
-		passwords.add("admin3");
-		passwords.add("admin4");
+		adminIDs.add("0001");
+		adminIDs.add("0010");
+		adminIDs.add("0011");
+		adminIDs.add("0100");
+		adminIDs.add("0101");
+		adminIDs.add("0110");
+		adminIDs.add("0111");		
+		
+		
+		adminPasswords.add("admin1111");
+		adminPasswords.add("admin2222");
+		adminPasswords.add("admin3333");
+		adminPasswords.add("admin4444");
+		adminPasswords.add("admin5555");
+		adminPasswords.add("admin6666");
+		adminPasswords.add("admin7777");
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Hello, please enter your password here: ");
+		System.out.print("Please enter your ID here: ");
+		
+		
+		String ID = scan.nextLine();
+		
+		while(!adminIDs.contains(ID)) {
+			System.out.print("No such ID, please try again: ");
+			ID = scan.nextLine();
+		}
+		
+		System.out.print("Please enter your password here: ");
 		
 		String pass = scan.nextLine();
 
-		while(!checkAdminPass(pass)) {
+		while(!checkAdminPass(ID,pass)) {
 			System.out.print("Wrong password, please try again: ");
 			pass = scan.nextLine();
 		}
 		
-		System.out.println("\nWelcome home admin");
+		System.out.println("\nWelcome home " + getName(ID));
 		
 	}
 }
