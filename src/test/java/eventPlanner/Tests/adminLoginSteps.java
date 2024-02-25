20747fc75e213a31d8960f25e538f38a2a00009c
PC <PC@DESKTOP-2H1NNT0>
2024-02-25 12:01:12 +0200

    Grouping adminLoginTest and adminLoginSteps

--- src/test/java/eventPlanner/Tests/adminLoginSteps.java
+++ src/test/java/eventPlanner/Tests/adminLoginSteps.java
@@ -1,5 +1,6 @@
-package event.Planner;
+package eventPlanner.Tests;
 
+import event.Planner.adminLoginMain;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
