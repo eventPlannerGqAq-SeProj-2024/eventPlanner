16178b128021f5b49f458f8a9c4044ae75f4b5c0
Adel Qadi <Msys@DESKTOP-O3P379O>
2024-04-01 21:32:02 +0300

    birthday/wedding updates

--- src/main/java/event/Planner/wedding.java
+++ src/main/java/event/Planner/wedding.java
@@ -1,5 +1,7 @@
 package event.Planner;
 
+import java.util.Scanner;
+
 public class wedding extends event {
 	
 	private String Bride,Groom,Officiant,Wedding_type,DJ,Cake;
@@ -61,4 +63,192 @@ public class wedding extends event {
 	{
 		return Cake;
 	};
+	
+	public void manageEvent()
+	{
+		Scanner scan = new Scanner (System.in);
+		System.out.println("choose what detail you want to change:");
+		System.out.println("1.name");
+		System.out.println("2.date");
+		System.out.println("3.theme");
+		System.out.println("4.description");
+		System.out.println("5.attendee count");
+		System.out.println("6.Officiant");
+		System.out.println("7.Cake");
+		System.out.println("8.Wedding_type");
+		System.out.println("9.DJ");
+		System.out.println("10.Bride");
+		System.out.println("11.Groom");
+		int cases= scan.nextInt();
+		String edit;
+		switch(cases)
+		{
+		case 1:
+		{
+			System.out.println("enter new name:");
+			super.setName(scan.next());
+			System.out.println("change successful!");
+		}
+		case 2:
+		{
+			System.out.println("enter new date:");
+			edit=scan.next();
+			if(super.getVenue().checkIfDateIsBooked(edit))
+			{
+				System.out.println("that date is booked");
+			}
+			else {super.setDate(edit);
+			System.out.println("change successful!");
+			}
+		}
+		case 3:
+		{
+			System.out.println("enter new theme:");
+			super.setTheme(scan.next());
+			System.out.println("change successful!");
+		}
+		case 4:
+		{
+			System.out.println("enter new description:");
+			super.setDescription(scan.next());
+			System.out.println("change successful!");
+		}
+		case 5:
+		{
+			System.out.println("enter new attendee count:");
+			super.setAttendee_count(scan.nextInt());
+			System.out.println("change successful!");
+		}
+		case 6:
+		{
+			System.out.println("enter new Officiant:");
+			Officiant =scan.next();
+			System.out.println("change successful!");
+		}
+		case 7:
+		{
+			System.out.println("enter new Cake:");
+			Cake=scan.next();
+			System.out.println("change successful!");
+		}
+		case 8:
+		{
+			System.out.println("enter new Wedding type:");
+			Wedding_type=scan.next();
+			System.out.println("change successful!");
+		}
+		case 9:
+		{
+			System.out.println("enter new DJ:");
+			DJ=scan.next();
+			System.out.println("change successful!");
+		}
+		case 10:
+		{
+			System.out.println("enter new Bride:");
+			Bride=scan.next();
+			System.out.println("change successful!");
+		}
+		case 11:
+		{
+			System.out.println("enter new Groom:");
+			Groom=scan.next();
+			System.out.println("change successful!");
+		}
+		}
+	}
+	public void manageEventTest(int cases,String edit,int count)
+	{
+	//	Scanner scan = new Scanner (System.in);
+		System.out.println("choose what detail you want to change:");
+		System.out.println("1.name");
+		System.out.println("2.date");
+		System.out.println("3.theme");
+		System.out.println("4.description");
+		System.out.println("5.attendee count");
+		System.out.println("6.Officiant");
+		System.out.println("7.Cake");
+		System.out.println("8.Wedding_type");
+		System.out.println("9.DJ");
+		System.out.println("10.Bride");
+		System.out.println("11.Groom");
+		
+	//	String edit;
+		switch(cases)
+		{
+		case 1:
+		{
+			System.out.println("enter new name:");
+			super.setName(edit);
+			System.out.println("change successful!");
+		}
+		case 2:
+		{
+			System.out.println("enter new date:");
+			//edit=edit;
+			if(super.getVenue().checkIfDateIsBooked(edit))
+			{
+				System.out.println("that date is booked");
+			}
+			else {super.setDate(edit);
+			System.out.println("change successful!");
+			}
+		}
+		case 3:
+		{
+			System.out.println("enter new theme:");
+			super.setTheme(edit);
+			System.out.println("change successful!");
+		}
+		case 4:
+		{
+			System.out.println("enter new description:");
+			super.setDescription(edit);
+			System.out.println("change successful!");
+		}
+		case 5:
+		{
+			System.out.println("enter new attendee count:");
+			super.setAttendee_count(count);
+			System.out.println("change successful!");
+		}
+		case 6:
+		{
+			System.out.println("enter new Officiant:");
+			Officiant =edit;
+			System.out.println("change successful!");
+		}
+		case 7:
+		{
+			System.out.println("enter new Cake:");
+			Cake=edit;
+			System.out.println("change successful!");
+		}
+		case 8:
+		{
+			System.out.println("enter new Wedding type:");
+			Wedding_type=edit;
+			System.out.println("change successful!");
+		}
+		case 9:
+		{
+			System.out.println("enter new DJ:");
+			DJ=edit;
+			System.out.println("change successful!");
+		}
+		case 10:
+		{
+			System.out.println("enter new Bride:");
+			Bride=edit;
+			System.out.println("change successful!");
+		}
+		case 11:
+		{
+			System.out.println("enter new Groom:");
+			Groom=edit;
+			System.out.println("change successful!");
+		}
+		}
+	}
+
 }
